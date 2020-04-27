8d2c8b353aed96d765bbf8636477b68d92acc908
Sala <elvisdenis056@gmail.com>
2020-04-27 13:24:01 +0300

    using call_command to run openCV from the browser

--- users/views.py
+++ users/views.py
@@ -5,31 +5,39 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.core.management import call_command
+
 
 # Create your views here.
 
 
 def Login(request):
-    return render(request, 'login.html', {})
+    return render(request, "login.html", {})
 
 
 # from .models import PassportPhoto
 
 
 def SignUp(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # throw that data into the form...
         form = DrowsyDriverUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             # save to create the user..
             user = form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
             # new_photo = PassportPhoto(photo=request.FILES['photo'])
             # new_photo.save()
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = DrowsyDriverUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, "signup.html", {"form": form})
+
+
+# Running an external python script..
+def RunOpenCV(request):
+    call_command("video")
+    return render(request, "home.html", {})
