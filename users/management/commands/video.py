2f280fdc3ea8030a0024b3a17f13e5605ee50bb8
Sala <elvisdenis056@gmail.com>
2020-04-30 16:06:44 +0300

    allow passing user as an argument in the custom command

--- users/management/commands/video.py
+++ users/management/commands/video.py
@@ -11,6 +11,9 @@ class Command(BaseCommand):
 
     help = "We are watching you"
 
+    def add_arguments(self, parser):
+        parser.add_argument("user", nargs="+", type=str)
+
     def handle(self, *args, **options):
         """Anything below is run as a custom command."""
-        vid()
+        vid(options["user"])
