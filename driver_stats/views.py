3b6a5b0ccc5efb61ba54c34980a1798283bdc9d5
Sala <elvisdenis056@gmail.com>
2020-05-04 16:46:56 +0300

    querying the db for the stats and filters by username, of the logged in user

--- driver_stats/views.py
+++ driver_stats/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from driver_stats.models import Stats
 
 # Create your views here.
+def statistics_page(request):
+    fetched_statistics = Stats.objects.filter(username=request.user.username)
+    context = {"fetched_statistics": fetched_statistics}
+
+    return render(request, "stats.html", context)
