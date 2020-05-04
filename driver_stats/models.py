7830834d253b5a3a57c3dbaf9c41b665b77bcc1f
Sala <elvisdenis056@gmail.com>
2020-05-04 16:46:09 +0300

    creating the statistics model

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Stats(models.Model):
+    first_name = models.CharField(max_length=10)
+    username = models.CharField(max_length=10)
+    eye_aspect_ratio = models.CharField(max_length=20)
+    time_alarm_raised = models.DateTimeField(auto_now_add=True)
+    car_registration_number = models.CharField(max_length=10, default="")
