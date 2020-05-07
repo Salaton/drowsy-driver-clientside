48928b11f07ee4c046ce593bc53831d2527d451a
Sala <elvisdenis056@gmail.com>
2020-05-07 11:08:46 +0300

    updated urls to deal with password changes and resets

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from . import views
 
 urlpatterns = [
@@ -7,4 +13,21 @@ urlpatterns = [
     path("home/", views.RunOpenCV, name="RunOpenCV"),
     path("profile/", views.edit_profile, name="edit_profile"),
     path("profile/", views.profile, name="profile"),
+    path("change-password/", views.change_password, name="change_password"),
+    path("reset-password/", PasswordResetView.as_view(), name="reset_password"),
+    path(
+        "reset-password/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset-password/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    re_path(
+        "reset-password/complete/$",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
