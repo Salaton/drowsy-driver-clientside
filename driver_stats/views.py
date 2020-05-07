54c0a0285a59b7df9d4c87b4e02a5a826250f007
Sala <elvisdenis056@gmail.com>
2020-05-07 11:02:36 +0300

    preventing unauthorized access

--- driver_stats/views.py
+++ driver_stats/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from driver_stats.models import Stats
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required()
 def statistics_page(request):
     fetched_statistics = Stats.objects.filter(username=request.user.username)
     context = {"fetched_statistics": fetched_statistics}
