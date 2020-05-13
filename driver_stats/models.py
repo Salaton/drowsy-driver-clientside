2d14b8885b5dc341389244f386ada70b3c4bb586
Sala <elvisdenis056@gmail.com>
2020-05-13 13:01:01 +0300

    Driver statistics model

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from users.models import DrowsyDriverUser
 
 # Create your models here.
 class Stats(models.Model):
-    first_name = models.CharField(max_length=10)
+    # user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15)
     username = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=15)
     eye_aspect_ratio = models.CharField(max_length=20)
     time_alarm_raised = models.DateTimeField(auto_now_add=True)
-    car_registration_number = models.CharField(max_length=10, default="")
+    car_registration_number = models.CharField(max_length=10)
+
+
+# class Stats(models.Model):
+#     user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE)
+#     eye_aspect_ratio = models.CharField(max_length=20)
+#     time_alarm_raised = models.DateTimeField(auto_now_add=True)
