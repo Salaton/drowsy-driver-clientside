df73fbd5c1fd2bf8ffb81529109349affe9b06b8
Sala <elvisdenis056@gmail.com>
2020-05-20 16:52:37 +0300

    add null=True to accept empty records

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -4,12 +4,12 @@ from users.models import DrowsyDriverUser
 # Create your models here.
 class Stats(models.Model):
     # user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=15)
-    username = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=15)
-    eye_aspect_ratio = models.CharField(max_length=20)
-    time_alarm_raised = models.DateTimeField(auto_now_add=True)
-    car_registration_number = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=15, null=True)
+    username = models.CharField(max_length=10, null=True)
+    last_name = models.CharField(max_length=15, null=True)
+    eye_aspect_ratio = models.CharField(max_length=20, null=True)
+    time_alarm_raised = models.DateTimeField(auto_now_add=True, null=True)
+    car_registration_number = models.CharField(max_length=10, null=True)
 
 
 # class Stats(models.Model):
