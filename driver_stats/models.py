3ad1052769b18854a1ff63a50b4105f717118e3d
Sala <elvisdenis056@gmail.com>
2020-05-20 09:09:43 +0300

    Adding a OneToOne field

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -3,13 +3,9 @@ from users.models import DrowsyDriverUser
 
 # Create your models here.
 class Stats(models.Model):
-    # user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=15)
-    username = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=15)
+    user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE, null=True)
     eye_aspect_ratio = models.CharField(max_length=20)
     time_alarm_raised = models.DateTimeField(auto_now_add=True)
-    car_registration_number = models.CharField(max_length=10)
 
 
 # class Stats(models.Model):
