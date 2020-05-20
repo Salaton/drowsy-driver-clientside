6a090f8997f65f4a309f7bda45455ec80f4806c0
Sala <elvisdenis056@gmail.com>
2020-05-20 20:36:20 +0300

    created a one to one relationship with the statistics table

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -3,9 +3,9 @@ from users.models import DrowsyDriverUser
 
 # Create your models here.
 class Stats(models.Model):
-    user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE, null=True)
-    eye_aspect_ratio = models.CharField(max_length=20)
-    time_alarm_raised = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(DrowsyDriverUser, on_delete=models.CASCADE, null=True)
+    eye_aspect_ratio = models.CharField(max_length=20, null=True)
+    time_alarm_raised = models.DateTimeField(auto_now_add=True, null=True)
 
 
 # class Stats(models.Model):
