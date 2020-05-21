bb4153fcf52ee550c99f56629336d0bf47398846
Sala <elvisdenis056@gmail.com>
2020-05-21 23:23:13 +0300

    add foreign key field

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -3,19 +3,9 @@ from users.models import DrowsyDriverUser
 
 # Create your models here.
 class Stats(models.Model):
-<<<<<<< HEAD
-    # user = models.OneToOneField(DrowsyDriverUser, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=15, null=True)
-    username = models.CharField(max_length=10, null=True)
-    last_name = models.CharField(max_length=15, null=True)
-    eye_aspect_ratio = models.CharField(max_length=20, null=True)
-    time_alarm_raised = models.DateTimeField(auto_now_add=True, null=True)
-    car_registration_number = models.CharField(max_length=10, null=True)
-=======
     user = models.ForeignKey(DrowsyDriverUser, on_delete=models.CASCADE, null=True)
     eye_aspect_ratio = models.CharField(max_length=20, null=True)
     time_alarm_raised = models.DateTimeField(auto_now_add=True, null=True)
->>>>>>> testing
 
 
 # class Stats(models.Model):
