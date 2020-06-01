7d06df7be53206ef418b9a2ae4ec2506107cf996
Sala <elvisdenis056@gmail.com>
2020-06-01 23:23:30 +0300

    removed property null=True from model

--- driver_stats/models.py
+++ driver_stats/models.py
@@ -3,7 +3,7 @@ from users.models import DrowsyDriverUser
 
 # Create your models here.
 class Stats(models.Model):
-    user = models.ForeignKey(DrowsyDriverUser, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(DrowsyDriverUser, on_delete=models.CASCADE)
     eye_aspect_ratio = models.CharField(max_length=20, null=True)
     time_alarm_raised = models.DateTimeField(auto_now_add=True, null=True)
 
